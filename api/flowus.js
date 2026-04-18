9392075bc07766f6ff0eada273f54c94262b3395
Uronika <151454297+Uronika@users.noreply.github.com>
2026-04-18 17:57:27 +0800

    Set minimal Vercel function baseline

--- api/flowus.js
+++ api/flowus.js
@@ -1,17 +1,8 @@
-// api/flowus.js
-import axios from 'axios';
-
-export default async function handler(req, res) {
-  const code = req.query.code || req.body.code;
-
-  console.log('Received OAuth code:', code);
-
-  // TODO: 使用 code 换取 FlowUs access token
-  // axios.post('https://flowus.cn/oauth/token', {...})
-
-  res.status(200).send(`<html><body>
-    <h2>授权完成！</h2>
-    <p>收到 code: ${code}</p>
-    <p>可以关闭浏览器回到客户端</p>
-  </body></html>`);
+export function GET(request) {
+  return new Response("FlowUs callback is alive", {
+    status: 200,
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8"
+    }
+  });
 }
